1a7ead4da9291620545c01bbff5f88d64ba37454
D20_TsT <76813856+R-203@users.noreply.github.com>
2021-08-23 17:29:04 +0700

    Add files via upload

---
+++ play.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+M='\033[1;31m'
+H='\033[1;32m'
+clear
+echo
+echo -e $M"Tunggu Sampai Proses Selesai Sabar Kawan...!"
+echo -e $H"Orang Sabar Disayang Janda  Xixixi:v"
+echo
+apt update && upgrade
+clear
+echo
+echo -e $M"Tunggu Sampai Proses Selesai Sabar Kawan...!"
+echo -e $H"Orang Sabar Disayang Janda  Xixixi:v"
+echo
+pkg install figlet -y
+clear
+echo
+echo -e $M"Tunggu Sampai Proses Selesai Sabar Kawan...!"
+echo -e $H"Orang Sabar Disayang Janda  Xixixi:v"
+echo
+pkg install toilet -y
+clear
+echo
+echo -e $M"Tunggu Sampai Proses Selesai Sabar Kawan...!"
+echo -e $H"Orang Sabar Disayang Janda  Xixixi:v"
+echo
+pkg install ruby -y
+clear
+echo
+echo -e $M"Tunggu Sampai Proses Selesai Sabar Kawan...!"
+echo -e $H"Orang Sabar Disayang Janda  Xixixi:v"
+echo
+gem install lolcat -y
+clear
+echo
+echo -e $M"Tunggu Sampai Proses Selesai Sabar Kawan...!"
+echo -e $H"Orang Sabar Disayang Janda  Xixixi:v"
+echo
+pkg install python -y
+clear
+echo
+echo -e $M"Tunggu Sampai Proses Selesai Sabar Kawan...!"
+echo -e $H"Orang Sabar Disayang Janda  Xixixi:v"
+echo
+pkg install python2 -y
+clear
+echo
+echo -e $M"Tunggu Sampai Proses Selesai Sabar Kawan...!"
+echo -e $H"Orang Sabar Disayang Janda  Xixixi:v"
+echo
+pip install requests
+clear
+echo
+echo -e $M"Tunggu Sampai Proses Selesai Sabar Kawan..!"
+echo -e $H"Orang Sabar Disayang Janda  Xixixi:v"
+echo
+pip2 install requests
+clear
+figlet R-203 | lolcat
+sleep 2
+clear
+figlet R-203 | lolcat
+sleep 2
+clear
+echo
+echo "╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮" | lolcat
+echo "╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯" | lolcat
+echo "┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈" | lolcat
+echo "┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈" | lolcat
+echo "╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮" | lolcat
+echo "╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯" | lolcat
+echo
+echo
+echo "Langsung ketik python filename or python2 filename Pada script👍" | lolcat
+
